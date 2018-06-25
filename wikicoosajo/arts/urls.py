759b17b4302f56a15dc2fcfca7c796a794caabfe
katuliza139 <katyliza139@gmail.com>
2018-06-25 12:44:57 -0600

    Add files via upload

--- wikicoosajo/arts/urls.py
+++ wikicoosajo/arts/urls.py
@@ -4,14 +4,14 @@ from django.views import *
 from django import forms
 
 urlpatterns = [
-    re_path(r'^articulo/crear/', CrearArticulo.as_view(), name="articulo"),
-    re_path(r'^articulo/(?P<arti>\d+)$',articulo_detalle,name="articulo_detalle"),
-    re_path(r'^articulo/palabra/$',buscar_por_pal, name="palabra_busqueda"),
-    re_path(r'^articulo/validar/(?P<artic>\d+)$',validar_articulo,name="articulo_validado"),
-    re_path(r'^articulo/busqueda/(?P<catg>\d+)$',buscar_por_categoria,name="buscar_por_categoria"),
-    re_path(r'^articulo/validar/$',articulo_validar, name="articulos_por_validar"),
-    re_path(r'^categoria/crear/$',CrearCategoria.as_view(),name="crear_categoria"),
-    re_path(r'^signup/$', usuarios.signup, name='signup'),
-    re_path(r'^articulo/notificaciones/$', notificaciones,name="notificaciones")
+    url(r'^articulo/crear/$', CrearArticulo.as_view(), name="articulo"),
+    url(r'^articulo/(?P<arti>\d+)$',articulo_detalle,name="articulo_detalle"),
+    url(r'^articulo/palabra/$',buscar_por_pal, name="palabra_busqueda"),
+    url(r'^articulo/validar/(?P<artic>\d+)$',validar_articulo,name="articulo_validado"),
+    url(r'^articulo/busqueda/(?P<catg>\d+)$',buscar_por_categoria,name="buscar_por_categoria"),
+    url(r'^articulo/validar/$',articulo_validar, name="articulos_por_validar"),
+    url(r'^categoria/crear/$',CrearCategoria.as_view(),name="crear_categoria"),
+    url(r'^signup/$', usuarios.signup, name='signup'),
+    url(r'^articulo/notificaciones/$', notificaciones,name="notificaciones")
 
 ]
